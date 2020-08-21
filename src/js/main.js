bdef23b076c38de61572823c0456af097edb621b
Thiago Carvalho <thiagotcs.dev@gmail.com>
2020-08-20 23:35:22 -0300

    cria pagina de login com estilos e validacoes

---
+++ src/js/main.js
@@ -0,0 +1,19 @@
+function signin(e) {
+  e.preventDefault();
+  const user = document.getElementById('user');
+  const password = document.getElementById('password');
+  const disclaimer = document.getElementsByClassName('signin__disclaimer');
+  const userMocked = 'email.usuario@compasso.com.br';
+  const passwordMocked = '123456';
+  if (user.value !== userMocked || password.value !== passwordMocked) {
+    user.classList.add('signin__input--error');
+    password.classList.add('signin__input--error');
+    disclaimer[0].style.visibility = 'visible';
+    disclaimer[1].style.visibility = 'visible';
+  } else {
+    user.classList.remove('signin__input--error');
+    password.classList.remove('signin__input--error');
+    disclaimer[0].style.visibility = 'hidden';
+    disclaimer[1].style.visibility = 'hidden';
+  }
+}
